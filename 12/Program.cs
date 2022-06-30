7c4384750fb93667d813da01f3ee966f79d6c1bc
Vyacheslavclear <mpk_optima-v@mail.ru>
2022-06-30 18:37:22 +0300

    12

---
+++ 12/Program.cs
@@ -0,0 +1,23 @@
+﻿int n;
+n = Convert.ToInt32(Console.ReadLine());
+if (n > 0 && n < 5){
+    if (n == 1){
+        Console.WriteLine("x = (0, +~)");
+        Console.WriteLine("y = (0, +~)");
+    }
+    if (n == 2){
+        Console.WriteLine("x = (-~, 0)");
+        Console.WriteLine("y = (0, +~)");
+    }
+    if (n == 3){
+        Console.WriteLine("x = (-~, 0)");
+        Console.WriteLine("y = (-~, 0)");
+    }
+    if (n == 4){
+        Console.WriteLine("x = (0, +~)");
+        Console.WriteLine("y = (-~, 0)");
+    }
+}
+else{
+    Console.WriteLine("Такой четверти не существует! Ты Врун!");
+}
