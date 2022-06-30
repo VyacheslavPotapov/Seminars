7437489eb1bef1a68e59e62987b37690972a2d91
Vyacheslavclear <mpk_optima-v@mail.ru>
2022-06-30 18:14:37 +0300

    8

---
+++ 8/Program.cs
@@ -0,0 +1,11 @@
+﻿int n1, n2;
+n1 = Convert.ToInt32(Console.ReadLine());
+n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 % n2 == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine(n1 % n2);
+}
