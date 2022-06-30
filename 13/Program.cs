04cd363dd31bc33106ea92d917aa1f732beb0f35
Vyacheslavclear <mpk_optima-v@mail.ru>
2022-06-30 18:40:26 +0300

    13

---
+++ 13/Program.cs
@@ -0,0 +1,19 @@
+﻿int x, y;
+x = Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0){
+    Console.WriteLine("x = (0, +~)");
+    Console.WriteLine("y = (0, +~)");
+}
+if (x < 0 && y > 0){
+    Console.WriteLine("x = (-~, 0)");
+    Console.WriteLine("y = (0, +~)");
+}
+if (x < 0 && y < 0){
+    Console.WriteLine("x = (-~, 0)");
+    Console.WriteLine("y = (-~, 0)");
+}
+if (x > 0 && y < 0){
+    Console.WriteLine("x = (0, +~)");
+    Console.WriteLine("y = (-~, 0)");
+}
