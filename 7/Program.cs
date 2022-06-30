da696d8bc18595d78e524bd2496b1ff5b6f4972a
Vyacheslavclear <mpk_optima-v@mail.ru>
2022-06-30 18:10:40 +0300

    7

---
+++ 7/Program.cs
@@ -0,0 +1,13 @@
+﻿int n1, n2, n;
+n = new Random().Next(9, 100);
+Console.WriteLine(n);
+n1 = n / 10;
+n2 = n % 10;
+if (n1 > n2)
+{
+    Console.WriteLine(n1);
+}
+else
+{
+    Console.WriteLine(n2);
+}
