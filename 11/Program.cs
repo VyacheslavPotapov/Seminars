4a66d8c9df1abb30fea2e1b4c1d1d322c42c9fd5
Vyacheslavclear <mpk_optima-v@mail.ru>
2022-06-30 18:33:50 +0300

    11

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,7 +1,7 @@
-﻿int n, i = 1;
-n = Convert.ToInt32(Console.ReadLine());
-while (i <= n)
-{
-    Console.WriteLine(i * i);
-    i++;
-}
+﻿double x, y, x1, y1, len;
+x = Convert.ToDouble(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+x1 = Convert.ToDouble(Console.ReadLine());
+y1 = Convert.ToDouble(Console.ReadLine());
+len = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+Console.WriteLine(len);
